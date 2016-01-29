3cd8e30c6824021fa710840ba95990d1aa001c0c
pcardoso <pcardoso@pcardoso-K56CB>
2016-01-29 15:48:16 +0000

    1º Programa. Hello world.

---
+++ 01.HelloWorld/src/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * The Class HelloWorld.
+ *
+ * @author pcardoso
+ * @version 0.1
+ */
+
+public class HelloWorld {
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World! my first program for 'Complementos de programacao'");
+	}
+}
