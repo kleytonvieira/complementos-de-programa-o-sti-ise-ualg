c99b012302589dd1440c7ea82b551bccde4a155f
pcardoso <pcardoso@ualg.pt>
2016-02-12 08:28:48 +0000

    04.vehicle: Added class Person and update of programs using it.

---
+++ 04.Vehicle/src/Person.java
@@ -0,0 +1,112 @@
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Person.
+ */
+public class Person {
+ 
+ /** The forename. */
+ String forename;
+ 
+ /**
+  * Gets the forename.
+  *
+  * @return the forename
+  */
+ public String getForename() {
+  return forename;
+}
+
+/**
+ * Sets the forename.
+ *
+ * @param forename the new forename
+ */
+public void setForename(String forename) {
+  this.forename = forename;
+}
+
+/**
+ * Gets the surname.
+ *
+ * @return the surname
+ */
+public String getSurname() {
+  return surname;
+}
+
+/**
+ * Sets the surname.
+ *
+ * @param surname the new surname
+ */
+public void setSurname(String surname) {
+  this.surname = surname;
+}
+
+/**
+ * Gets the address.
+ *
+ * @return the address
+ */
+public String getAddress() {
+  return address;
+}
+
+/**
+ * Sets the address.
+ *
+ * @param address the new address
+ */
+public void setAddress(String address) {
+  this.address = address;
+}
+
+/**
+ * Gets the cc.
+ *
+ * @return the cc
+ */
+public String getCC() {
+  return CC;
+}
+
+/**
+ * Sets the cc.
+ *
+ * @param cC the new cc
+ */
+public void setCC(String cC) {
+  CC = cC;
+}
+
+/**
+ * Gets the phone number.
+ *
+ * @return the phone number
+ */
+public String getPhoneNumber() {
+  return phoneNumber;
+}
+
+/**
+ * Sets the phone number.
+ *
+ * @param phoneNumber the new phone number
+ */
+public void setPhoneNumber(String phoneNumber) {
+  this.phoneNumber = phoneNumber;
+}
+
+/** The surname. */
+String surname;
+ 
+ /** The address. */
+ String address;
+ 
+ /** The cc. */
+ String CC;
+ 
+ /** The phone number. */
+ String phoneNumber;
+}
---
+++ 04.Vehicle/src/Program2.java
@@ -0,0 +1,30 @@
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Program2.
+ */
+public class Program2 {
+  
+  /**
+   * The main method.
+   *
+   * @param args the arguments
+   */
+  public static void main(String[] args) {
+    Person p1 = new Person();
+    p1.setForename("Margarida");
+    p1.setSurname("Herdeiro");
+    p1.setAddress("Rua das flores, lote 1, Faro");
+    p1.setCC("238921739");
+    p1.setPhoneNumber("289800100");    
+    Car car = new Car();
+    car.setOwner(p1);
+    System.out.println("The owner of the car is " + car.getOwner());
+    
+    if (car.setCarKms(-100)) {
+      System.out.println("kms setted");
+    } else {
+      System.out.println("Error while setting kms");
+    }
+  }
+}
