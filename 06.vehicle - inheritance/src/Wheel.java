01606394bf95355e94804b3dcb77ad7ed1e12d58
Pedro J.S. Cardoso <pcardoso@ualg.pt>
2016-02-24 16:07:54 +0000

    06. vehicle heritance: programa test

--- 06.vehicle - inheritance/src/Wheel.java
+++ 06.vehicle - inheritance/src/Wheel.java
@@ -6,70 +6,150 @@
  */
 public class Wheel {
 	
-	public int getWidth() {
-		return width;
-	}
+	/** The construction. */
+	private String construction; //e.g., R -radial, B -Bias 
 
-	public void setWidth(int width) {
+	/** The height. */
+	private int height;
+
+	/** The load index. */
+	private int loadIndex;
+
+	/**
+	 * Instantiates a new wheel.
+	 *
+	 * @param construction the construction
+	 * @param height the height
+	 * @param loadIndex the load index
+	 * @param rimDiameter the rim diameter
+	 * @param speedRating the speed rating
+	 * @param width the width
+	 */
+	public Wheel(String construction, int height, int loadIndex, int rimDiameter, String speedRating, int width) {
+		super();
+		this.construction = construction;
+		this.height = height;
+		this.loadIndex = loadIndex;
+		this.rimDiameter = rimDiameter;
+		this.speedRating = speedRating;
 		this.width = width;
 	}
 
-	public int getHeight() {
-		return height;
-	}
+	/** The rim diameter. */
+	private int rimDiameter;
 
-	public void setHeight(int height) {
-		this.height = height;
-	}
+	/** The speed rating. */
+	private String speedRating; //e.g., V	
 
-	public int getRimDiameter() {
-		return rimDiameter;
-	}
+	/** The width. */
+	private int width;
 
-	public void setRimDiameter(int rimDiameter) {
-		this.rimDiameter = rimDiameter;
-	}
-
+	/**
+	 * Gets the construction.
+	 *
+	 * @return the construction
+	 */
 	public String getConstruction() {
 		return construction;
 	}
 
-	public void setConstruction(String construction) {
-		this.construction = construction;
+	/**
+	 * Gets the height.
+	 *
+	 * @return the height
+	 */
+	public int getHeight() {
+		return height;
 	}
 
+	/**
+	 * Gets the load index.
+	 *
+	 * @return the load index
+	 */
 	public int getLoadIndex() {
 		return loadIndex;
 	}
 
-	public void setLoadIndex(int loadIndex) {
-		this.loadIndex = loadIndex;
+	/**
+	 * Gets the rim diameter.
+	 *
+	 * @return the rim diameter
+	 */
+	public int getRimDiameter() {
+		return rimDiameter;
 	}
 
+	/**
+	 * Gets the speed rating.
+	 *
+	 * @return the speed rating
+	 */
 	public String getSpeedRating() {
 		return speedRating;
 	}
 
+	/**
+	 * Gets the width.
+	 *
+	 * @return the width
+	 */
+	public int getWidth() {
+		return width;
+	}
+
+	/**
+	 * Sets the construction.
+	 *
+	 * @param construction the new construction
+	 */
+	public void setConstruction(String construction) {
+		this.construction = construction;
+	}
+	
+	/**
+	 * Sets the height.
+	 *
+	 * @param height the new height
+	 */
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	/**
+	 * Sets the load index.
+	 *
+	 * @param loadIndex the new load index
+	 */
+	public void setLoadIndex(int loadIndex) {
+		this.loadIndex = loadIndex;
+	}
+	
+	/**
+	 * Sets the rim diameter.
+	 *
+	 * @param rimDiameter the new rim diameter
+	 */
+	public void setRimDiameter(int rimDiameter) {
+		this.rimDiameter = rimDiameter;
+	}
+	
+	/**
+	 * Sets the speed rating.
+	 *
+	 * @param speedRating the new speed rating
+	 */
 	public void setSpeedRating(String speedRating) {
 		this.speedRating = speedRating;
 	}
-
-	/** The width. */
-	private int width;
 	
-	/** The height. */
-	private int height;
-	
-	/** The rim diameter. */
-	private int rimDiameter;
-	
-	/** The construction. */
-	private String construction; //e.g., R -radial, B -Bias 
-	
-	/** The load index. */
-	private int loadIndex;
-	
-	/** The speed rating. */
-	private String speedRating; //e.g., V	
+	/**
+	 * Sets the width.
+	 *
+	 * @param width the new width
+	 */
+	public void setWidth(int width) {
+		this.width = width;
+	}
 	
 }
