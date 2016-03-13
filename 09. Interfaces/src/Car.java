77e0b926db3d23f0f9efe2ed103dfdd05631627c
pcardoso <pcardoso@ualg.pt>
2016-03-13 23:55:48 +0000

    0.9 added de insurance interface

--- 09. Interfaces/src/Car.java
+++ 09. Interfaces/src/Car.java
@@ -1,8 +1,11 @@
+import java.util.Date;
 
-public class Car extends LandVehicle implements CirculationTax {
+public class Car extends LandVehicle implements CirculationTax, Insurance {
   
   private int   numberOfDoor;
   private float carAnualTax;
+  private Date  insuranceDate;
+  private float insurance;
                 
   public Car(String brand, String model, float price, float landVelocity, int numberOfWheels, int numberOfDoor) {
     super(brand, model, price, landVelocity, numberOfWheels);
@@ -33,4 +36,26 @@ public class Car extends LandVehicle implements CirculationTax {
   public void setAnualTax(float tax) {
     this.carAnualTax = tax;
   }
+  
+  @Override
+  public void setDate(Date date) {
+    this.insuranceDate = date;
+  }
+  
+  @Override
+  public Date getDate() {
+    return this.insuranceDate;
+  }
+  
+  @Override
+  public void setInsurenaceValue(float value) {
+    this.insurance = value;
+    
+  }
+  
+  @Override
+  public float getInsuranceValue() {
+    return this.insurance;
+  }
+  
 }
