ee55cd5b2654692b8c438f1dbec462c2468c47e4
Pedro Cardoso <pcardoso@ualg.pt>
2016-04-28 23:12:09 +0100

    12. Database- connection examples

---
+++ 12. database/src/ConnToSQLite.java
@@ -0,0 +1,53 @@
+//STEP 1. Import required packages
+import java.sql.*;
+
+public class ConnToSQLite {
+	// JDBC driver name and database URL
+	static final String JDBC_DRIVER = "org.sqlite.JDBC";  
+	static final String DB_URL = "jdbc:sqlite:test.sqlite";
+
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		Statement stmt = null;
+		try{
+			//STEP 2: Register JDBC driver
+			Class.forName(JDBC_DRIVER);
+
+			//STEP 3: Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL);
+
+			//STEP 4: Execute a query
+			System.out.println("Quering database...");
+			stmt = conn.createStatement();
+
+			ResultSet rs = stmt.executeQuery("SELECT * FROM Animals");
+
+			while(rs.next()){
+				System.out.println(rs.getString("AnimalName"));
+			}
+			System.out.println("Database queried successfully...");
+		}catch(SQLException se){
+			//Handle errors for JDBC
+			se.printStackTrace();
+		}catch(Exception e){
+			//Handle errors for Class.forName
+			e.printStackTrace();
+		}finally{
+			//finally block used to close resources
+			try{
+				if(stmt!=null)
+					stmt.close();
+			}catch(SQLException se2){
+			}// nothing we can do
+			try{
+				if(conn!=null)
+					conn.close();
+			}catch(SQLException se){
+				se.printStackTrace();
+			}//end finally try
+		}//end try
+		System.out.println("Goodbye!");
+	}//end main
+}//end JDBCExample		 
