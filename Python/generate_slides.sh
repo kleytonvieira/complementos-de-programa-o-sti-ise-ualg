9d63ced2ee5fbc79b97224e6e976810c31198b84
Pedro Cardoso <pcardoso@ualg.pt>
2020-03-03 11:43:14 +0000

    gerados os html a partir dos notebooks de POO

--- Python/generate_slides.sh
+++ Python/generate_slides.sh
@@ -1,5 +1,6 @@
-if [ $# -eq 0 ]
-then
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
   echo "Usage: $0 lista_de_ficheiros"
   echo "e.g.,"
   echo "    $0 POO-jupyter/*.ipynb"
@@ -8,5 +9,6 @@ fi
 for f in $*
 do
   echo $f
-  jupyter-nbconvert $f --to slides # --post serve
+#   jupyter-nbconvert $f --to slides --SlidesExporter.reveal_scroll=True 
+  jupyter-nbconvert $f --to html 
 done
